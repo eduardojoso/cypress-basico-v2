5d422e525754a39f41347083c277126a2d23acfc
eferrero <eduardo.ferrero@capgemini.com>
2023-10-01 12:12:27 -0300

    Revert "Quebrando algo de proposito"
    This reverts commit 15d545378540fdb6b93a92c83207b35d46bb6c0d.

--- src/script.js
+++ src/script.js
@@ -59,7 +59,7 @@ function showAndHideErrorMessage() {
   const errorMessage = document.querySelector('.error')
   errorMessage.style.display = 'block'
   scroll(0,0)
-  hideMessagAfterTimeout(errorMessage)
+  hideMessageAfterTimeout(errorMessage)
   return
 }
 
